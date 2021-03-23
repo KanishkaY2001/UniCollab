0b59671b951dd38043ff0dcbc70f6d80e1aa9545
Christina <xxychristina@gmail.com>
2021-03-23 12:12:43 +0800

    login page

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,14 +1,35 @@
 <template>
-  <header>
-    <div class="text-box">
-      <h1>Student List</h1>
-      <p class="mt-3">love uni ❤️ ️</p>
-      <v-btn to="/students">
-        View studentList <span class="ml-2">&rarr;</span>
+  <div>
+    <v-row cols="12">
+      <v-col cols="10" style="margin-left: 10px">
+        <img class="logo1 ml-5" src="/img/logo.svg"/>
+      </v-col>
+      <v-btn style="margin-top: 20px; margin-right: 20px" color="#55CBD3" dark elevation="0">
+        LOGIN
       </v-btn>
-    </div>
-  </header>
+    </v-row>
+    <v-row>
+      <div class="welcome"> 
+        <div class="welcome-text">Welcome to UniCollab!</div>
+      </div>
+      <div style="height: 300px"></div>
+    </v-row>
+    <v-row align="center" justify="space-around">
+      <div style="height: 50px; width: 100%">
+        <div class="sign-up">Sign-up to start collaborating</div>
+      </div>
+    </v-row>
+    <v-row>
+       <div style="height: 50px; width: 100%">
+        <v-btn color="#55CBD3" dark elevation="0" style="width: 150px;margin-left: -75px;left: 50%">
+          GET STARTED
+        </v-btn>
+      </div>
+    </v-row>
+  </div>
 </template>
+
+
 <script>
 export default {
   head() {
@@ -18,32 +39,33 @@ export default {
   },
 };
 </script>
-<style>
-header {
-  min-height: 100vh;
-  background-image: linear-gradient(
-      to right,
-      rgba(0, 0, 0, 0.9),
-      rgba(0, 0, 0, 0.4)
-    ),
-    url("/images/banner.jpg");
-  background-position: center;
-  background-size: cover;
-  position: relative;
+
+<style scoped lang="scss">
+.logo1 {
+  left: 5%;
+  width: 80px;
+  height: 80px;
 }
-.text-box {
-  position: absolute;
-  top: 50%;
-  left: 10%;
-  transform: translateY(-50%);
-  color: #fff;
+.welcome {
+  height: 150px;
+  width: 100%;
+  background-color: #A1EEF4;
+  .welcome-text {
+    font-size: 25px;
+    color: white;
+    font-weight: bold;
+    text-align: center;
+    vertical-align: middle;
+    line-height: 150px;
+  }
 }
-.text-box h1 {
-  font-family: cursive;
-  font-size: 5rem;
+.sign-up {
+  color: grey;
+  text-align: center;
 }
 .text-box p {
-  font-size: 2rem;
+  color: grey;
+  font-size: 8px;
   font-weight: lighter;
 }
 </style>
