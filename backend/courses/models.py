bfd3a593c03bbcb24226231553276d4f7ea72373
Christina <xxychristina@gmail.com>
2021-04-21 03:26:46 +0800

    add course data

--- backend/courses/models.py
+++ backend/courses/models.py
@@ -4,6 +4,10 @@ from django.db import models
 class Course(models.Model):
     name = models.CharField(max_length=140, unique=True)
     info = models.CharField(max_length=10000, default=True, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
     class Meta:
         ordering = ['name']
 
